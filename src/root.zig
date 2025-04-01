59311d2d098ee3d07bb7b9b777f217ed28ccd684
s0und0fs1lence <goncear97@gmail.com>
2025-04-01 18:37:14 +0000

    remove code from root.zig

--- src/root.zig
+++ src/root.zig
@@ -4,20 +4,9 @@
 const std = @import("std");
 const testing = std.testing;
 
-const chdb = @cImport({
-    @cInclude("/workspaces/chdb-zig/header/chdb.h");
-});
 pub export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
-
-pub const ChConn = struct {
-    conn: chdb.chdb_conn,
-    alloc: std.mem.Allocator,
-    pub fn new(self: *ChConn, alloc: std.mem.Allocator) *ChConn {
-        self.alloc = alloc;
-    }
-};
 
 test "basic add functionality" {
     try testing.expect(add(3, 7) == 10);
